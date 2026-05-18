6d23d3c3555757c67f95c5f9634bb4da0732c4bb
Your Name <you@example.com>
2026-05-18 19:02:52 +0300

    Fix based badge position

--- app/page.tsx
+++ app/page.tsx
@@ -1190,16 +1190,16 @@ onClick={() => {
       </div>
 
       {/* FOOTER */}
-<footer className="fixed bottom-6 left-1/2 -translate-x-1/2 z-[99999]">
+<footer className="flex justify-center mt-14 md:mt-10">
 
   
     {/* BASE STATUS */}
-    <div className="flex items-center gap-1 bg-white/[0.05] px-7 py-3.5 rounded-full border border-white/10 backdrop-blur-xl shadow-xl">
+    <div className="flex items-center gap-1 bg-white/[0.05] px-5 py-3.5 rounded-full border border-white/10 backdrop-blur-xl shadow-xl">
       <span className="text-[11px] text-blue-500 font-black tracking-widest uppercase italic leading-none">
         You&apos;re now based
       </span>
 
-      <div className="relative w-14 h-4 flex items-center">
+      <div className="relative w-5 h-4 flex items-center">
         <Image
           src="/base_logo.png"
           alt="Base Logo"
