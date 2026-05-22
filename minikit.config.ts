67873098b8dd116675e44ac51d9bfa3e91edd383
Your Name <you@example.com>
2026-05-22 16:28:29 +0300

    fix farcaster root url

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,7 +1,4 @@
-const ROOT_URL =
-  process.env.NEXT_PUBLIC_URL ||
-  (process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`) ||
-  "http://localhost:3000";
+const ROOT_URL = "https://mini.basedoracle.space";
 
 /**
  * MiniApp configuration object. Must follow the mini app manifest specification.
