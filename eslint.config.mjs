a1af35be1c5c9c1154a460e28935225d4300ea36
Your Name <you@example.com>
2026-06-27 16:48:45 +0300

    fix: eslint rules off

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,13 +13,9 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-        },
-      ],
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off",
+      "react-hooks/exhaustive-deps": "off",
     },
   },
 ];
